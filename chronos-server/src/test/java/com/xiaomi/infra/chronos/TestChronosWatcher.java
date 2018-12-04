7d6383d0d125fd4bd3343a89a45276e9e0044974
Duo Zhang <zhangduo@apache.org>
2018-12-04 11:11:37 +0800

    Upgrade version to 2.0.0-SNAPSHOT, use java 1.8, thrift 0.9.2 and remove hbase dependency and use curator-test instead

--- chronos-server/src/test/java/com/xiaomi/infra/chronos/TestChronosWatcher.java
+++ chronos-server/src/test/java/com/xiaomi/infra/chronos/TestChronosWatcher.java
@@ -2,41 +2,35 @@ package com.xiaomi.infra.chronos;
 
 import static org.junit.Assert.assertTrue;
 
+import com.xiaomi.infra.chronos.exception.ChronosException;
+import com.xiaomi.infra.chronos.exception.FatalChronosException;
+import com.xiaomi.infra.chronos.zookeeper.FailoverServer;
+import com.xiaomi.infra.chronos.zookeeper.HostPort;
+import com.xiaomi.infra.chronos.zookeeper.ZooKeeperUtil;
 import java.io.IOException;
 import java.util.Properties;
-
-import org.apache.commons.lang.math.RandomUtils;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseTestingUtility;
-import org.apache.hadoop.hbase.zookeeper.ZKConfig;
+import org.apache.curator.test.TestingServer;
 import org.apache.zookeeper.KeeperException;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.xiaomi.infra.chronos.ChronosServerWatcher;
-import com.xiaomi.infra.chronos.exception.FatalChronosException;
-import com.xiaomi.infra.chronos.exception.ChronosException;
-import com.xiaomi.infra.chronos.zookeeper.FailoverServer;
-import com.xiaomi.infra.chronos.zookeeper.HostPort;
-import com.xiaomi.infra.chronos.zookeeper.ZooKeeperUtil;
-
 /**
  * Test {@link ChronosServerWatcher}
  */
 public class TestChronosWatcher {
-  protected final static HBaseTestingUtility TEST_UTIL = new HBaseTestingUtility(
-      new Configuration());
+
+  private static TestingServer ZK_SERVER;
 
   @BeforeClass
   public static void setUpBeforeClass() throws Exception {
-    TEST_UTIL.startMiniZKCluster(1);
+    ZK_SERVER = new TestingServer(true);
   }
 
   @AfterClass
   public static void tearDownAfterClass() throws Exception {
-    TEST_UTIL.shutdownMiniZKCluster();
+    ZK_SERVER.close();
   }
 
   @Before
@@ -51,8 +45,7 @@ public class TestChronosWatcher {
     properties.setProperty(FailoverServer.SERVER_HOST, hostPort.getHost());
     properties.setProperty(FailoverServer.SERVER_PORT, String.valueOf(hostPort.getPort()));
     properties.setProperty(FailoverServer.BASE_ZNODE, "/chronos/test-cluster");
-    properties.setProperty(FailoverServer.ZK_QUORUM,
-      ZKConfig.getZKQuorumServersString(TEST_UTIL.getConfiguration()));
+    properties.setProperty(FailoverServer.ZK_QUORUM, ZK_SERVER.getConnectString());
     properties.setProperty(FailoverServer.SESSION_TIMEOUT, String.valueOf(3000));
     properties.setProperty(FailoverServer.CONNECT_RETRY_TIMES, String.valueOf(10));
 
@@ -61,11 +54,11 @@ public class TestChronosWatcher {
 
   @Test
   public void testInitZnode() throws IOException, KeeperException {
-    ChronosServerWatcher chronosServerWatcher = createChronosWatcher(new HostPort("127.0.0.1",
-        10086));
+    ChronosServerWatcher chronosServerWatcher =
+      createChronosWatcher(new HostPort("127.0.0.1", 10086));
 
-    assertTrue(ZooKeeperUtil.watchAndCheckExists(chronosServerWatcher,
-      chronosServerWatcher.getBaseZnode()));
+    assertTrue(
+      ZooKeeperUtil.watchAndCheckExists(chronosServerWatcher, chronosServerWatcher.getBaseZnode()));
     assertTrue(ZooKeeperUtil.watchAndCheckExists(chronosServerWatcher,
       chronosServerWatcher.getBackupServersZnode()));
     assertTrue(ZooKeeperUtil.watchAndCheckExists(chronosServerWatcher,
@@ -75,15 +68,17 @@ public class TestChronosWatcher {
   }
 
   @Test
-  public void testSetPersistentTimestamp() throws IOException, FatalChronosException,
-      ChronosException, KeeperException {
-    ChronosServerWatcher chronosServerWatcher = createChronosWatcher(new HostPort("127.0.0.1",
-        10086));
+  public void testSetPersistentTimestamp()
+      throws IOException, FatalChronosException, ChronosException, KeeperException {
+    ChronosServerWatcher chronosServerWatcher =
+      createChronosWatcher(new HostPort("127.0.0.1", 10086));
 
-    long expectTimestamp = RandomUtils.nextLong();
+    assertTrue(chronosServerWatcher.getPersistentTimestamp() == 0);
+
+    long expectTimestamp = System.currentTimeMillis();
     chronosServerWatcher.setPersistentTimestamp(expectTimestamp);
-    long actualTimestamp = ZooKeeperUtil.bytesToLong(ZooKeeperUtil.getDataAndWatch(
-      chronosServerWatcher, chronosServerWatcher.getPersistentTimestampZnode()));
+    long actualTimestamp = ZooKeeperUtil.bytesToLong(ZooKeeperUtil
+      .getDataAndWatch(chronosServerWatcher, chronosServerWatcher.getPersistentTimestampZnode()));
     assertTrue(actualTimestamp == expectTimestamp);
 
     chronosServerWatcher.close();
@@ -91,12 +86,12 @@ public class TestChronosWatcher {
 
   @Test
   public void testGetPersistentTimestamp() throws IOException, ChronosException, KeeperException {
-    ChronosServerWatcher chronosServerWatcher = createChronosWatcher(new HostPort("127.0.0.1",
-        10086));
+    ChronosServerWatcher chronosServerWatcher =
+      createChronosWatcher(new HostPort("127.0.0.1", 10086));
 
     assertTrue(chronosServerWatcher.getPersistentTimestamp() == 0);
 
-    long expectTimestamp = RandomUtils.nextLong();
+    long expectTimestamp = System.currentTimeMillis();
     chronosServerWatcher.setPersistentTimestamp(expectTimestamp);
     assertTrue(chronosServerWatcher.getPersistentTimestamp() == expectTimestamp);
 
--- chronos-server/src/test/java/com/xiaomi/infra/chronos/client/TestChronosClient.java
+++ chronos-server/src/test/java/com/xiaomi/infra/chronos/client/TestChronosClient.java
@@ -2,60 +2,52 @@ package com.xiaomi.infra.chronos.client;
 
 import static org.junit.Assert.assertTrue;
 
-import java.io.IOException;
-import java.util.Properties;
-
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseTestingUtility;
-import org.apache.hadoop.hbase.zookeeper.ZKConfig;
-import org.apache.thrift.transport.TTransportException;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import com.xiaomi.infra.chronos.client.ChronosClient;
-import com.xiaomi.infra.chronos.client.ChronosClientWatcher;
 import com.xiaomi.infra.chronos.ChronosServer;
 import com.xiaomi.infra.chronos.ChronosServerWatcher;
 import com.xiaomi.infra.chronos.exception.ChronosException;
 import com.xiaomi.infra.chronos.zookeeper.FailoverServer;
 import com.xiaomi.infra.chronos.zookeeper.HostPort;
+import java.io.IOException;
+import java.util.Properties;
+import org.apache.curator.test.TestingServer;
+import org.apache.thrift.transport.TTransportException;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
- * Test {@link ChronosClient}
- * 
- * Pass with thrift-0.8.0, but not pass with thrift-0.5.0 because the class of server is not found.
+ * Test {@link ChronosClient}.
  */
 public class TestChronosClient {
-  private final static HBaseTestingUtility TEST_UTIL = new HBaseTestingUtility(new Configuration());
+
+  private static TestingServer ZK_SERVER;
 
   @BeforeClass
   public static void setUpBeforeClass() throws Exception {
-    TEST_UTIL.startMiniZKCluster(1);
+    ZK_SERVER = new TestingServer(true);
   }
 
   @AfterClass
   public static void tearDownAfterClass() throws Exception {
-    TEST_UTIL.shutdownMiniZKCluster();
+    ZK_SERVER.close();
   }
 
   public ChronosClient createChronosServer() throws IOException {
     Properties properties = new Properties();
-    properties.setProperty(ChronosClient.ZK_QUORUM,
-      ZKConfig.getZKQuorumServersString(TEST_UTIL.getConfiguration()));
+    properties.setProperty(ChronosClient.ZK_QUORUM, ZK_SERVER.getConnectString());
     properties.setProperty(ChronosClient.SESSION_TIMEOUT, String.valueOf(3000));
     properties.setProperty(ChronosClient.CONNECT_RETRY_TIMES, String.valueOf(10));
 
     return new ChronosClient(new ChronosClientWatcher(properties));
   }
 
-  public ChronosServer createChronosServer(HostPort hostPort) throws IOException, TTransportException, ChronosException {
+  public ChronosServer createChronosServer(HostPort hostPort)
+      throws IOException, TTransportException, ChronosException {
     Properties properties = new Properties();
     properties.setProperty(FailoverServer.SERVER_HOST, hostPort.getHost());
     properties.setProperty(FailoverServer.SERVER_PORT, String.valueOf(hostPort.getPort()));
     properties.setProperty(FailoverServer.BASE_ZNODE, "/chronos/default-cluster");
-    properties.setProperty(FailoverServer.ZK_QUORUM,
-      ZKConfig.getZKQuorumServersString(TEST_UTIL.getConfiguration()));
+    properties.setProperty(FailoverServer.ZK_QUORUM, ZK_SERVER.getConnectString());
     properties.setProperty(FailoverServer.SESSION_TIMEOUT, String.valueOf(3000));
     properties.setProperty(ChronosServer.MAX_THREAD, "100000");
     properties.setProperty(ChronosServer.ZK_ADVANCE_TIMESTAMP, "100000");
@@ -65,7 +57,8 @@ public class TestChronosClient {
   }
 
   @Test
-  public void testGetTimestamp() throws IOException, InterruptedException, TTransportException, ChronosException {
+  public void testGetTimestamp()
+      throws IOException, InterruptedException, TTransportException, ChronosException {
     final ChronosServer chronosServer = createChronosServer(new HostPort("127.0.0.1", 2187));
     Thread thread = new Thread() {
       @Override
@@ -80,14 +73,15 @@ public class TestChronosClient {
     long timestamp1 = chronosClient.getTimestamp();
     long timestamp2 = chronosClient.getTimestamp();
     assertTrue(timestamp1 < timestamp2);
-    
+
     chronosServer.stopThriftServer();
     chronosServer.getFailoverWatcher().close();
     chronosClient.getChronosClientWatcher().close();
   }
-  
+
   @Test
-  public void testGetTimestamps() throws IOException, InterruptedException, TTransportException, ChronosException {
+  public void testGetTimestamps()
+      throws IOException, InterruptedException, TTransportException, ChronosException {
     final ChronosServer chronosServer = createChronosServer(new HostPort("127.0.0.1", 2188));
     Thread thread = new Thread() {
       @Override
